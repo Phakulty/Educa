80ee034178e90d65329ea843b0aa35eda106c6c5
Phakulty <anathonrichards23@gmail.com>
2025-06-13 13:13:01 +0100

    refactor(Navbar): remove redundant shadow class from header and nav elements

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
   };
 
   return (
-    <header className="bg-[#ddd] shadow-md py-4">
+    <header className="bg-[#ddd] py-4">
       <div className="container mx-auto flex justify-between items-center px-4">
         {/* Logo */}
         <h1>
@@ -65,7 +65,7 @@ const Navbar = () => {
         <nav
           className={`${
             isMenuOpen ? 'translate-x-0' : '-translate-x-full'
-          } fixed top-0 left-0 w-64 h-full bg-[#ddd] shadow-lg z-50 transform transition-transform duration-300 ease-in-out md:static md:w-auto md:translate-x-0 md:flex md:items-center`}
+          } fixed top-0 left-0 w-64 h-full bg-[#ddd] z-50 transform transition-transform duration-300 ease-in-out md:static md:w-auto md:translate-x-0 md:flex md:items-center`}
         >
           <ul className="flex flex-col md:flex-row md:space-x-6 space-y-6 md:space-y-0 p-6 md:p-2">
             <li>
